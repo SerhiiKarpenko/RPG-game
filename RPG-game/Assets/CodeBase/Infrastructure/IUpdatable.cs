03b305fcbdef41bb8df2b84677c8855c34be922c
Serhii Karpenko <SerhiiKarpenko@users.noreply.github.com>
2022-12-02 18:43:13 +0100

    Added IUpdatable interface
     IUpdatable added for States to make them Updatable

---
+++ RPG-game/Assets/CodeBase/Infrastructure/IUpdatable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace CodeBase.Infrastructure
+{
+	public interface IUpdatable
+	{
+		IEnumerator Update(Action action)
+		{
+			while (true)
+			{
+				action?.Invoke();
+				yield return null;
+			}	
+		}
+	}
+}
