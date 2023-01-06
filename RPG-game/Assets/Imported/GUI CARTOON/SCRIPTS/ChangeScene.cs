d3bda2dc3a13ea63e7891e5d601578ab9e26466c
Serhii Karpenko <SerhiiKarpenko@users.noreply.github.com>
2023-01-06 11:04:51 +0100

    Added GUI cartoon package

---
+++ RPG-game/Assets/Imported/GUI CARTOON/SCRIPTS/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string NameScene = "GameScene";
+
+	public void ChangueScene(){
+		GetComponent<AudioSource>().Play();
+		Invoke("GameNameScene", GetComponent<AudioSource>().clip.length);
+	}
+	
+	
+	void GameNameScene(){
+		Application.LoadLevel (NameScene);
+	}
+}
