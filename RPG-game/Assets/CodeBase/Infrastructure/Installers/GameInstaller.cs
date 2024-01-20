877855a1a37ec9caa4f019d1c79483874053ca98
Serhii Karpenko <80330854+SerhiiKarpenko@users.noreply.github.com>
2024-01-20 21:28:26 +0100

    Added game installer

---
+++ RPG-game/Assets/CodeBase/Infrastructure/Installers/GameInstaller.cs
@@ -0,0 +1,13 @@
+using Zenject;
+
+namespace CodeBase.Infrastructure.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            //register dependencies here            
+            
+        }
+    }
+}
